511ccb35d1b63d85c6e8859be4f00be79492aa83
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-04-29 10:42:00 +0900

    프로젝트 초기 파일

---
+++ Source/MyUIWave/MyUIWave.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyUIWave.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyUIWave, "MyUIWave" );
