497464baa73dbcbd8acefb29d2fa7803d8e067ff
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-04-29 20:45:48 +0900

    폭발 반경을 적용할 두 번째 콜리전 추가, TimerHandler를 추가, TimerHandler를 이용하여 정해진 시간 뒤 두 번째 콜리전 내부에 있는 액터 확인 후 데미지를 입히는 함수 추가

--- Source/MyUIWave/MyExplosiveMineItem.cpp
+++ Source/MyUIWave/MyExplosiveMineItem.cpp
@@ -9,11 +9,38 @@ AMyExplosiveMineItem::AMyExplosiveMineItem()
 	DelayTime = 5.0f;
 	ExplosionRadius = 300.0f;
 
-	USphereComponent* Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("Collision"));
-	InitCollision(Sphere);	// Box Collision 컴포넌트 생성 -> Scene 컴포넌트에 부착, StaticMesh를 Collision에 다시 부착
+	ActivationCollision = CreateDefaultSubobject<USphereComponent>(TEXT("Collision"));
+	InitCollision(ActivationCollision);	
+	// Sphere Collision 컴포넌트 생성 -> Scene 컴포넌트에 부착, StaticMesh를 Collision에 다시 부착
+
+	ExplosionCollision = CreateDefaultSubobject<USphereComponent>(TEXT("ExplosionCollision"));
+	InitCollision(ExplosionCollision);
+	ExplosionCollision->InitSphereRadius(ExplosionRadius);
 }
 
 void AMyExplosiveMineItem::ActivateItem(AActor* Activator)
 {
-	Destroy();
+	GetWorld()->GetTimerManager().SetTimer(
+		ExplosionTimerHandle,	// ExplosionTimerHandle 핸들러를
+		this,	// 이 객체에서 가져오고
+		&AMyExplosiveMineItem::Explode,	// AMyExplosiveMineItem::Explode 함수를
+		DelayTime,	// DelayTime 초 뒤에
+		false	// 한 번만 실행 (<-> true : 반복)
+	);
+}
+
+void AMyExplosiveMineItem::Explode()
+{
+	TArray<AActor*> OverlappingActors;	// Overlap 되어있는 Actor를 담을 TArray
+	ExplosionCollision->GetOverlappingActors(OverlappingActors);	// Overlap 되어있는 Actor를 TArray에 넣기
+
+	for (AActor* Actor : OverlappingActors) // TArray 원소 순회
+	{
+		if (Actor && Actor->ActorHasTag("Player"))	// Actor가 유효하고, "Player" 태그가 있으면
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 2, FColor::Green, FString::Printf(TEXT("Get %d Damage"), DamageAmount));
+		}
+	}
+
+	DestroyItem();
 }
--- Source/MyUIWave/MyExplosiveMineItem.h
+++ Source/MyUIWave/MyExplosiveMineItem.h
@@ -4,6 +4,8 @@
 #include "MyOverlapTrapItem.h"
 #include "MyExplosiveMineItem.generated.h"
 
+class USphereComponent;
+
 UCLASS()
 class MYUIWAVE_API AMyExplosiveMineItem : public AMyOverlapTrapItem
 {
@@ -12,7 +14,14 @@ class MYUIWAVE_API AMyExplosiveMineItem : public AMyOverlapTrapItem
 public:
 	AMyExplosiveMineItem();
 
+	USphereComponent* ActivationCollision;
+	USphereComponent* ExplosionCollision;
+
+	FTimerHandle ExplosionTimerHandle;	// 폭발 시간을 관리할 타이머 핸들러
+
 	virtual void ActivateItem(AActor* Activator) override;
+
+	void Explode();
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|OverlapTrap")
 	float ExplosionRadius;
