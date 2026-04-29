259f9762d9018dd0957829287c87706b2bb9fd12
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-04-29 14:42:36 +0900

    리플렉션 추가

--- Source/MyUIWave/MyOverlapConsumableItem.h
+++ Source/MyUIWave/MyOverlapConsumableItem.h
@@ -1,14 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "MyBaseItem.h"
 #include "MyOverlapConsumableItem.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class MYUIWAVE_API AMyOverlapConsumableItem : public AMyBaseItem
 {
@@ -20,5 +15,7 @@ public:
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|OverlapConsumable")
 	int32 HPAmount;		// 아이템 획득 시 HP 변화량
-	int32 SpeedAmount;	// 아이템 획득 시 이동 속도 변화량
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|OverlapConsumable")
+	float SpeedAmount;	// 아이템 획득 시 이동 속도 변화량
 };
