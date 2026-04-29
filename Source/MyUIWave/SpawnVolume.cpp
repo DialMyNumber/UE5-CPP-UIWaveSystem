391fed0e12d2dbebb85704543f6bf2be35c01723
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-04-30 00:17:04 +0900

    데이터 테이블을 가져와 확률에 따라 어떤 열의 아이템을 호출할지 결정

--- Source/MyUIWave/SpawnVolume.cpp
+++ Source/MyUIWave/SpawnVolume.cpp
@@ -10,6 +10,8 @@ ASpawnVolume::ASpawnVolume()
 
 	SpawningBox = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawningBox"));
 	SpawningBox->SetupAttachment(Scene);
+
+	ItemDataTable = nullptr;	// ItemDataTable 초기화
 }
 
 FVector ASpawnVolume::GetRandomPointInVolume() const
@@ -35,4 +37,62 @@ void ASpawnVolume::SpawnItem(TSubclassOf<AActor> ItemClass)
 		GetRandomPointInVolume(),	// 위치
 		FRotator::ZeroRotator	// 회전할지 여부
 	);
+}
+
+FMyItemSpawnRow* ASpawnVolume::GetRandomItem() const
+{
+	if(!ItemDataTable) return nullptr;
+	// 아이템 데이터 테이블이 유효하지 않으면 nullptr 반환 권장
+
+	TArray<FMyItemSpawnRow*> AllRows;	// 각 Row의 시작 주소를 담아두는 TArray
+	static const FString ContextString(TEXT("ItemSpawnContext"));
+	// 디버깅용 인자
+
+	// ItemDataTable의 모든 Row를 가져와서 AllRows TArray에 저장
+	ItemDataTable->GetAllRows(ContextString, AllRows); // 디버깅 용으로 첫번째 인자가 필요함
+	// 오류가 발생했을때 이 디버깅용 인자 텍스트가 포함된 문자열이 메시지나 로그에 나타남
+
+	if (AllRows.IsEmpty()) return nullptr;
+
+	float TotalChance = 0.0f;
+	for (const FMyItemSpawnRow* Row : AllRows)	// 차례대로 순회하면서
+	{
+		if (Row)	// Row가 유효할 때
+		{
+			TotalChance += Row->SpawnChance;	// 스폰 확률을 더함
+		}
+	}
+
+	// A = 50%, B = 20%, C = 10%, D = 20% 라고 했을때
+	// 0 ~ 50 | 50 ~ 70 | 70 ~ 80 | 80 ~ 100
+	// RandValue 의 값이 어떤 구간에 있는지를 확인하고 해당 구간의 아이템 스폰
+	// 스폰을 했다면 0으로 초기화를 해줘야함 --- (다음 함수 호출시 초기화)
+
+	const float RandValue = FMath::FRandRange(0.0f, TotalChance);
+	float AccumulateChance = 0.0f;
+
+	for (FMyItemSpawnRow* Row : AllRows) 
+	{
+		AccumulateChance += Row->SpawnChance;	// 해당 열의 스폰 확률
+		if (RandValue <= AccumulateChance)
+		{
+			return Row;
+		}
+	}
+
+	return nullptr;	// 혹시라도 위 for문에서 return이 되지 않았다면 nullptr 반환
+}
+
+
+void ASpawnVolume::SpawnRandomItem()
+{
+	// 임의의 Row(행)을 가져옴
+	if (FMyItemSpawnRow* SelectedRow = GetRandomItem())
+	{
+		// SelectedRow 의 ItemClass열을 Get() 하는데 TSoftClassPtr 타입으로 가져오기 때문에 UClass* 로 받아야함
+		if (UClass* ActualClass = SelectedRow->ItemClass.Get())
+		{
+			SpawnItem(ActualClass);	// 직접 구현했던 SpawnItem 함수로 스폰
+		}
+	}
 }
--- Source/MyUIWave/SpawnVolume.h
+++ Source/MyUIWave/SpawnVolume.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "MyItemSpawnRow.h"		// 아이템 테이블
 #include "SpawnVolume.generated.h"
 
 class UBoxComponent;
@@ -18,13 +19,22 @@ public:
 	USceneComponent* Scene;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spawning")
-	UBoxComponent* SpawningBox;
+	UBoxComponent* SpawningBox;	// 스폰 범위를 지정할 Box Component
 
-	UFUNCTION(BlueprintCallable, Category = "Spawning")
+	// 객체를 바꿔야하기 때문에 EditAnywhere
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawning")
+	UDataTable* ItemDataTable;	// 아이템 데이터 테이블
+
+	// UFUNCTION(BlueprintCallable, Category = "Spawning") // 테스트 정도에만 필요
 	FVector GetRandomPointInVolume() const;		// Getter라서 const로 읽기 전용
 	// 랜덤 FVector를 반환
 
-	UFUNCTION(BlueprintCallable, Category = "Spawning")
+	// UFUNCTION(BlueprintCallable, Category = "Spawning") // 테스트 정도에만 임시로 사용
 	void SpawnItem(TSubclassOf<AActor> ItemClass);	// TSubclassOf : 해당 클래스 포함 하위 클래스만 허용
-	// Spawn Item
+
+	FMyItemSpawnRow* GetRandomItem() const;
+	// 읽기 전용으로 const, 딱히 UFUNCTION() 해주지 않아도 됨
+
+	UFUNCTION(BlueprintCallable, Category = "Spawning")	// 테스트 정도에만 임시로 사용
+	void SpawnRandomItem();
 };
