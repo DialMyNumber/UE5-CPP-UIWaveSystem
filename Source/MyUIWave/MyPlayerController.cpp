3cb361287083cf070988f088bfcfe1f4c6a9b385
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-04-30 15:56:50 +0900

    메인 메뉴 UI, 메인 메뉴의 Start/Restart를 통해 게임 재시작

--- Source/MyUIWave/MyPlayerController.cpp
+++ Source/MyUIWave/MyPlayerController.cpp
@@ -1,7 +1,10 @@
 #include "MyPlayerController.h"
 #include "MyGameState.h"		// UpdateHUD(); 를 쓰기 위해
+#include "MyGameInstance.h"		// StartGame()에 사용
 #include "EnhancedInputSubsystems.h"
 #include "Blueprint/UserWidget.h"
+#include "Kismet/GameplayStatics.h"	// StartGame()에 사용
+#include "Components/TextBlock.h"	//	MainMenu에 사용
 
 AMyPlayerController::AMyPlayerController()
 	: InputMappingContext(nullptr),	// 어차피 블루프린트에서 할당할거긴 하지만 안전하게 nullptr로 초기화
@@ -9,7 +12,10 @@ AMyPlayerController::AMyPlayerController()
 	JumpAction(nullptr),
 	LookAction(nullptr),
 	SprintAction(nullptr),
-	HUDWidgetInstance(nullptr)
+	HUDWidgetClass(nullptr),
+	HUDWidgetInstance(nullptr),
+	MainMenuWidgetClass(nullptr),
+	MainMenuWidgetInstance(nullptr)
 {
 }
 
@@ -55,5 +61,91 @@ void AMyPlayerController::BeginPlay()
 // 이 함수를 이용해서 갱신은 Game State에서
 UUserWidget* AMyPlayerController::GetHUDWidget() const
 {
-	return HUDWidgetInstance; 
+	return HUDWidgetInstance;
+}
+
+void AMyPlayerController::ShowMainMenu(bool bIsRestart)
+{
+	if (HUDWidgetInstance)	// 이미 화면이 켜져있으면
+	{
+		HUDWidgetInstance->RemoveFromParent();	// 뷰포트에서 떼어내기
+		HUDWidgetInstance = nullptr;
+	}
+
+	if (MainMenuWidgetInstance)	// 이미 화면이 켜져있으면
+	{
+		MainMenuWidgetInstance->RemoveFromParent();	// 뷰포트에서 떼어내기
+		MainMenuWidgetInstance = nullptr;
+	}
+
+	if (MainMenuWidgetClass)	// 메인 메뉴 클래스가 있으면
+	{
+		MainMenuWidgetInstance = CreateWidget<UUserWidget>(this, MainMenuWidgetClass);	// 메인 메뉴 인스턴스 생성
+		if (MainMenuWidgetInstance)
+		{
+			MainMenuWidgetInstance->AddToViewport();	// 뷰포트에 MainMenuWidgetInstance 띄우기
+
+			bShowMouseCursor = true;			// 마우스 커서를 보이게 설정
+			SetInputMode(FInputModeUIOnly());	// 메인 메뉴상에 있을때 마우스의 포커스를 UI에만 작동하도록
+		}
+
+		if (UTextBlock* ButtonText = Cast<UTextBlock>(MainMenuWidgetInstance->GetWidgetFromName(TEXT("StartButtonText"))))
+		{
+			if (bIsRestart)
+			{
+				ButtonText->SetText(FText::FromString(TEXT("Restart")));
+			}
+			else
+			{
+				ButtonText->SetText(FText::FromString(TEXT("Start")));
+			}
+		}
+	}
+}
+
+
+void AMyPlayerController::ShowGameHUD()
+{
+	if (HUDWidgetInstance)	// 이미 화면이 켜져있으면
+	{
+		HUDWidgetInstance->RemoveFromParent();	// 뷰포트에서 떼어내기
+		HUDWidgetInstance = nullptr;
+	}
+
+	if (MainMenuWidgetInstance)	// 이미 화면이 켜져있으면
+	{
+		MainMenuWidgetInstance->RemoveFromParent();	// 뷰포트에서 떼어내기
+		MainMenuWidgetInstance = nullptr;
+	}
+
+	if (HUDWidgetClass)	// 메인 메뉴 클래스가 있으면
+	{
+		HUDWidgetInstance = CreateWidget<UUserWidget>(this, MainMenuWidgetClass);	// 메인 메뉴 인스턴스 생성
+		if (HUDWidgetInstance)
+		{
+			HUDWidgetInstance->AddToViewport();	// 뷰포트에 HUDWidgetInstance 띄우기 = 인게임 내
+
+			bShowMouseCursor = false;			// 마우스 커서를 안보이게 설정
+			SetInputMode(FInputModeGameOnly());	// 게임 내부에서만 마우스가 사용되도록(IA_Look)
+		}
+
+		// 삼항연산자
+		AMyGameState* MyGameState = GetWorld() ? GetWorld()->GetGameState<AMyGameState>() : nullptr;
+		if (MyGameState)
+		{
+			MyGameState->UpdateHUD();	// HUD 갱신
+		}
+	}
+}
+
+void AMyPlayerController::StartGame()
+{
+	// 게임 인스턴스를 완전히 초기화하기 위해
+	if (UMyGameInstance* MyGameInstance = Cast<UMyGameInstance>(UGameplayStatics::GetGameInstance(this)))
+	{
+		MyGameInstance->CurrentLevelIndex = 0;
+		MyGameInstance->TotalScore = 0;
+	}
+
+	UGameplayStatics::OpenLevel(GetWorld(), FName("BasicLevel"));
 }
