8a08083d40884092af31934961d8b96079589c39
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-04-30 15:38:06 +0900

    PlayerController에 위젯 추가

--- Source/MyUIWave/MyPlayerController.cpp
+++ Source/MyUIWave/MyPlayerController.cpp
@@ -1,12 +1,15 @@
 #include "MyPlayerController.h"
+#include "MyGameState.h"		// UpdateHUD(); 를 쓰기 위해
 #include "EnhancedInputSubsystems.h"
+#include "Blueprint/UserWidget.h"
 
 AMyPlayerController::AMyPlayerController()
 	: InputMappingContext(nullptr),	// 어차피 블루프린트에서 할당할거긴 하지만 안전하게 nullptr로 초기화
 	MoveAction(nullptr),
 	JumpAction(nullptr),
 	LookAction(nullptr),
-	SprintAction(nullptr)
+	SprintAction(nullptr),
+	HUDWidgetInstance(nullptr)
 {
 }
 
@@ -29,4 +32,28 @@ void AMyPlayerController::BeginPlay()
 			}
 		}
 	}
+
+	// Build.cs 에 추가가 필요함
+	if (HUDWidgetClass) // 갱신은 Game State에서
+	{
+		// this = PlayerController에 HUDWidgetClass를 만들어서 HUDWidget에 할당
+		HUDWidgetInstance = CreateWidget<UUserWidget>(this, HUDWidgetClass);
+		if (HUDWidgetInstance)
+		{
+			HUDWidgetInstance->AddToViewport();	// 뷰포트에 추가
+		}
+	}
+
+	// 삼항연산자, GetWorld() 성공시 GetWorld()->GetGameState<AMyGameState>()
+	AMyGameState* MyGameState = GetWorld() ? GetWorld()->GetGameState<AMyGameState>() : nullptr;
+	if (MyGameState)
+	{
+		MyGameState->UpdateHUD();
+	}
+}
+
+// 이 함수를 이용해서 갱신은 Game State에서
+UUserWidget* AMyPlayerController::GetHUDWidget() const
+{
+	return HUDWidgetInstance; 
 }
--- Source/MyUIWave/MyPlayerController.h
+++ Source/MyUIWave/MyPlayerController.h
@@ -6,6 +6,7 @@
 
 class UInputMappingContext;	// 헤더에선 전방선언
 class UInputAction;	// 헤더에선 전방선언
+class UUserWidget;
 
 UCLASS()
 class MYUIWAVE_API AMyPlayerController : public APlayerController
@@ -26,6 +27,30 @@ public:
 	UInputAction* LookAction;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
 	UInputAction* SprintAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "HUD")
+	TSubclassOf<UUserWidget> HUDWidgetClass;	// HUD
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "HUD")
+	UUserWidget* HUDWidgetInstance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Menu")
+	TSubclassOf<UUserWidget> MainMenuWidgetClass;	// 메인 메뉴 HUD
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Menu")
+	UUserWidget* MainMenuWidgetInstance;	// 메인 메뉴 HUD
+
+	UFUNCTION(BlueprintPure, Category = "HUD")
+	UUserWidget* GetHUDWidget() const;
+
+	UFUNCTION(BlueprintCallable, Category = "HUD")
+	void ShowGameHUD();		// HUD 불러오기
+
+	UFUNCTION(BlueprintCallable, Category = "Menu")
+	void ShowMainMenu(bool bIsRestart);	// 메인 메뉴 HUD, 게임 도중에 불러온지를 체크하기 위한 bool 변수
+
+	UFUNCTION(BlueprintCallable, Category = "Menu")
+	void StartGame();		// 시작, 재시작 담당
 
 protected:
 	virtual void BeginPlay() override;	// BeginPlay에서 활성화 할 예정
