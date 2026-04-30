6d605d8c8d8f8d6fd672bda532a30aed1f3b384f
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-04-30 11:41:50 +0900

    이동속도 변화 지속시간 추가

--- Source/MyUIWave/MyHealthPackItem.cpp
+++ Source/MyUIWave/MyHealthPackItem.cpp
@@ -6,7 +6,8 @@ AMyHealthPackItem::AMyHealthPackItem()
 	// 부모 클래스에서 초기화 하지 않았던 값들은 자식 클래스 생성자에서 초기화
 	ItemType = "HealthPack";
 	HPAmount = 30;		// 아이템 획득 시 HP 변화량
-	SpeedAmount = 0.0f;	// 아이템 획득 시 이동 속도 변화량
+	SpeedAmount = 0.0f;		// 아이템 획득 시 이동 속도 변화량
+	SpeedDuration = 0.0f;	// 아이템 획득 시 이동 속도 변화 지속시간
 
 	USphereComponent* Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("Collision"));
 	InitCollision(Sphere);
--- Source/MyUIWave/MySpeedBoostFeatherItem.cpp
+++ Source/MyUIWave/MySpeedBoostFeatherItem.cpp
@@ -6,7 +6,8 @@ AMySpeedBoostFeatherItem::AMySpeedBoostFeatherItem()
 	// 부모 클래스에서 초기화 하지 않았던 값들은 자식 클래스 생성자에서 초기화
 	ItemType = "SpeedBoostFeather";
 	HPAmount = 0;		// 아이템 획득 시 HP 변화량
-	SpeedAmount = 30.0f;	// 아이템 획득 시 이동 속도 변화량
+	SpeedAmount = 500.0f;	// 아이템 획득 시 이동 속도 변화량
+	SpeedDuration = 10.0f;	// 아이템 획득 시 이동 속도 변화 지속시간
 
 	USphereComponent* Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("Collision"));
 	InitCollision(Sphere);
