7f6441dacf033fe9d2fcba3fba4e33996f0e3b89
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-04-30 13:48:57 +0900

    GameState에서 - 게임 시작 - 게임 레벨 전환 - 게임 종료 - MyCoinItem과 Point 상호작용 관리

--- Source/MyUIWave/MyGameState.cpp
+++ Source/MyUIWave/MyGameState.cpp
@@ -1,8 +1,23 @@
 #include "MyGameState.h"
+#include "Kismet/GameplayStatics.h"
+#include "SpawnVolume.h"
+#include "MyCoinItem.h"			// 코인 아이템을 다 먹었는지 체크할 때 필요
 
 AMyGameState::AMyGameState()
 {
 	Score = 0;	// 스코어 초기화
+	SpawnedCoinCount = 0;	// 코인 초기화
+	CollectedCoinCount = 0;	// 코인 초기화
+	LevelDuration = 30.0f;	// 레벨 지속시간 초기화
+	CurrentLevelIndex = 0;	// 현재 레벨 초기화
+	MaxLevels = 3;		// 최대 레벨 초기화
+}
+
+void AMyGameState::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StartLevel();
 }
 
 int32 AMyGameState::GetScore() const
@@ -13,4 +28,93 @@ int32 AMyGameState::GetScore() const
 void AMyGameState::AddScore(int32 Amount)
 {
 	Score += Amount;
+}
+
+void AMyGameState::OnCoinCollected()
+{
+	++CollectedCoinCount;
+	UE_LOG(LogTemp, Warning, TEXT("Collected Coin : %d / %d"), CollectedCoinCount, SpawnedCoinCount);
+
+	if (SpawnedCoinCount > 0 && CollectedCoinCount >= SpawnedCoinCount)
+	{
+		EndLevel();
+	}
+}
+
+void AMyGameState::StartLevel()
+{
+	SpawnedCoinCount = 0;		// 현재 레벨별로 다시 초기화
+	CollectedCoinCount = 0;		// 현재 레벨별로 다시 초기화
+
+	TArray<AActor*> FoundVolumes;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ASpawnVolume::StaticClass(), FoundVolumes);
+	// GetWorld()를 통해 현재 월드에서, SpawnVolume을 찾아서, FoundVolumes에 넣음
+	// SpawnVolume이 여러개일수 있으니까
+
+	const int32 ItemToSpawn = 40;
+
+	// 스폰된 코인의 개수 카운트
+	for (int32 i = 0; i < ItemToSpawn; ++i) 
+	{
+		if (FoundVolumes.Num() > 0)	// SpawnVolume이 있을때
+		{
+			ASpawnVolume* SpawnVolume = Cast<ASpawnVolume>(FoundVolumes[0]);
+			if (SpawnVolume)
+			{
+				AActor* SpawnedActor = SpawnVolume->SpawnRandomItem();
+				// 
+
+				// AMyCoinItem 포함 그 하위 자식 클래스인지 확인
+				if (SpawnedActor && SpawnedActor->IsA(AMyCoinItem::StaticClass()))
+				{
+					++SpawnedCoinCount;
+				}
+			}
+		}
+	}
+
+	// 현재 레벨 제한시간 작동
+	GetWorldTimerManager().SetTimer(
+		LevelTimerHandle,
+		this,
+		&AMyGameState::OnLevelTimeUp,
+		LevelDuration,
+		false	// 반복여부
+	);
+
+	UE_LOG(LogTemp, Warning, TEXT("Level %d Start!, Spawned %d coin:"),
+		CurrentLevelIndex + 1,
+		SpawnedCoinCount);
+}
+
+void AMyGameState::OnLevelTimeUp()
+{
+	EndLevel();
+}
+
+// 시간 제한이 다 되었거나, 코인을 다 먹었거나 등
+void AMyGameState::EndLevel()
+{
+	GetWorldTimerManager().ClearTimer(LevelTimerHandle);	// 타이머 초기화
+	++CurrentLevelIndex;
+
+	if (CurrentLevelIndex >= MaxLevels)
+	{
+		OnGameOver();
+		return;
+	}
+
+	if (LevelMapsNames.IsValidIndex(CurrentLevelIndex))
+	{
+		UGameplayStatics::OpenLevel(GetWorld(), LevelMapsNames[CurrentLevelIndex]);
+	}
+	else // 예외처리
+	{
+		OnGameOver();
+	}
+}
+
+void AMyGameState::OnGameOver()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Game Over"));
 }
--- Source/MyUIWave/MyGameState.h
+++ Source/MyUIWave/MyGameState.h
@@ -4,6 +4,13 @@
 #include "GameFramework/GameState.h"
 #include "MyGameState.generated.h"
 
+
+// 레벨에 Row값을 기준으로 40개의 아이템 소환
+// 레벨이 시작되면 시간 제한 타이머 시작
+// 소환된 아이템 중 코인 아이템을 다 먹었을 경우 시간 제한에 상관 없이 다음 레벨로 이동
+// ExplosiveMine 같은 아이템으로 인해 체력이 0이 되면 즉시 게임 오버
+// UI - [게임 종료] 이후 처음 레벨부터 [UI - 재시작] 버튼을 누르면 재시작
+
 UCLASS()
 class MYUIWAVE_API AMyGameState : public AGameState
 {
@@ -12,12 +19,43 @@ class MYUIWAVE_API AMyGameState : public AGameState
 public:
 	AMyGameState();
 
+	virtual void BeginPlay() override;	// 게임을 시작할 때 초기화
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Score")
 	int32 Score;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Coin")
+	int32 SpawnedCoinCount = 0;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Coin")
+	int32 CollectedCoinCount = 0;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Level")
+	float LevelDuration;	// 현재 레벨 지속시간
+
+	FTimerHandle LevelTimerHandle;	// 현재 레벨의 타이머
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Level")
+	int32 MaxLevels;	// 최고 레벨 단계
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Level")
+	int32 CurrentLevelIndex;	// 현재 레벨 단계
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Level")
+	TArray<FName> LevelMapsNames;
 
 	UFUNCTION(BlueprintPure, Category = "Score")
 	int32 GetScore() const;
 
 	UFUNCTION(BlueprintCallable, Category = "Score")
 	void AddScore(int32 Amount);
+
+
+	void OnCoinCollected();
+	void StartLevel();
+	void OnLevelTimeUp();
+	void EndLevel();
+
+	UFUNCTION(BlueprintCallable, Category = "Level")
+	void OnGameOver();
 };
